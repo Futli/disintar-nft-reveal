f5e13c91d39450f8ab17b20669844d050d40e48f
Alexey Filonchikov <falex@au.ru>
2024-10-12 13:05:49 +0700

    rename

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
-import {TonConnectButton} from "@tonconnect/ui-react";
-import './header.scss';
+import { TonConnectButton } from "@tonconnect/ui-react";
+import "./header.scss";
 
 export const Header = () => {
-    return <header>
-        <span>Disintar Withdraw NFT</span>
-        <TonConnectButton />
+  return (
+    <header>
+      <span>Disintar Reveal NFT</span>
+      <TonConnectButton />
     </header>
-}
+  );
+};
